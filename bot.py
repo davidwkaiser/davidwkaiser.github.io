05d8128bb5a9e9766f4723bcd7ae7cce7e1bf28b
Dave Kaiser <davidwkaiser@yahoo.com>
2020-06-26 16:07:20 -0500

    get basic sms functionality working, refactored index.html

--- bot.py
+++ bot.py
@@ -4,5 +4,17 @@ from flask import url_for, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template("index.html")
+def index():
+	return render_template("index.html")
+
+@app.route('/iframe')
+def iframe():
+    return render_template("iframe.html")
+
+@app.route('/messenger')
+def messenger():
+    return render_template("messenger.html")    
+
+# @app.route('/assistant')
+# def assistant():
+#     return render_template("assistant.html")   
